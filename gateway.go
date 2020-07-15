7de425e1c0d9a96bcc11cef08cf9ab9af7ca6c23
Youxing Jiang <jiangyouxing@mozat.com>
2020-07-15 17:50:47 +0800

    add gateway

---
+++ gateway.go
@@ -0,0 +1,20 @@
+package easycall
+
+type Gateway struct {
+}
+
+func NewGateway(endpoints []string) (*Gateway, error) {
+	return nil, nil
+}
+
+func (gw *Gateway) CreateServer(port int) {
+
+}
+
+func (gw *Gateway) CreateHttpServer(port int) {
+
+}
+
+func (gw *Gateway) AddMiddleware() {
+
+}
