8a212e4e7521dda8ff729a206f78c226615c6787
Youxing Jiang <jiangyouxing@mozat.com>
2020-07-20 09:38:19 +0800

    change parameter body to reqBody

--- easy_client.go
+++ easy_client.go
@@ -34,13 +34,13 @@ func (ec *EasyClient) Request(serviceName string, method string, reqBody interfa
 	return respPkg.DecodeBody(respBody)
 }
 
-func (ec *EasyClient) RequestAsync(serviceName string, method string, body interface{}, timeout time.Duration) (chan *EasyPackage, error) {
-	return ec.RequestAsyncWithHead(FORMAT_MSGPACK, NewEasyHead().SetService(serviceName).SetMethod(method), body, timeout)
+func (ec *EasyClient) RequestAsync(serviceName string, method string, reqBody interface{}, timeout time.Duration) (chan *EasyPackage, error) {
+	return ec.RequestAsyncWithHead(FORMAT_MSGPACK, NewEasyHead().SetService(serviceName).SetMethod(method), reqBody, timeout)
 }
 
-func (ec *EasyClient) RequestWithHead(format byte, head *EasyHead, body interface{}, timeout time.Duration) (*EasyPackage, error) {
+func (ec *EasyClient) RequestWithHead(format byte, head *EasyHead, reqBody interface{}, timeout time.Duration) (*EasyPackage, error) {
 
-	ch, err := ec.RequestAsyncWithHead(format, head, body, timeout)
+	ch, err := ec.RequestAsyncWithHead(format, head, reqBody, timeout)
 
 	if err != nil {
 		return nil, err
